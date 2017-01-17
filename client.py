e1cf2bba63807232b9c2b87409d52977b539a035
LanceMaverick <pnoel6@gmail.com>
2017-01-17 16:51:03 +0100

    first commit

---
+++ client.py
@@ -0,0 +1,48 @@
+import requests
+
+class NoAPIKeyError:
+    pass
+
+def check_auth(func):
+    def inner(*args, **kwargs):
+        if not self.key:
+            raise NoAPIKeyError('No api key set')
+
+class BeardApiClient:
+    def __init__(self, **kwargs):
+        url = kwargs['url']
+        if 'http://' not in url:
+            self.base_url = 'http://'+url
+        else:
+            self.base_url = url
+        self.base_url = kwargs['url']
+        self.key = kwargs.get('key', None)
+        self.default_chat = kwargs.get('default_chat', None)
+    
+    #@check_auth
+    def send_message(self, text, chat_id = None):
+        if not chat_id:
+            try:
+                chat_id = self.default_chat
+            except ValueError as e:
+                print('No chat_id or default_chat defined')
+                raise(e)
+        payload = {
+                'chat_id': str(chat_id),
+                'text': text
+                }
+        url = '{}/key{}/relay/sendMessage'.format(
+                self.base_url,
+                self.key)
+        print(url)
+        response = requests.post(url, json = payload)
+        return response
+                
+
+    
+
+
+ 
+
+
+
